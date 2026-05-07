c1bb73919b5e065440fe76fd995961b20ada21ef
Davidlohr Bueso <dave@stgolabs.net>
2026-05-07 04:29:13 -0700

    locking/rtmutex: Skip remove_waiter() when waiter is not enqueued
    syzbot triggered the following splat in remove_waiter() via
    FUTEX_CMP_REQUEUE_PI:
    
      KASAN: null-ptr-deref in range [0x0000000000000a88-0x0000000000000a8f]
       class_raw_spinlock_constructor
       remove_waiter+0x159/0x1200 kernel/locking/rtmutex.c:1561
       rt_mutex_start_proxy_lock+0x103/0x120
       futex_requeue+0x10e4/0x20d0
       __x64_sys_futex+0x34f/0x4d0
    
    task_blocks_on_rt_mutex() does not arm the waiter upon deadlock detection,
    leaving waiter->task nil, where 3bfdc63936dd ("rtmutex: Use waiter::task instead
    of current in remove_waiter()") made this fatal.
    
    Furthermore, rt_mutex_start_proxy_lock() should not be calling into remove_waiter()
    upon a successfully grabbing the rtmutex. 1a1fb985f2e2 ("futex: Handle early deadlock
    return correctly"), moved the remove_waiter() out of __rt_mutex_start_proxy_lock()
    (where 'ret' was only ever 0 or < 0) into the wrapper. Tighten this check to
    account for try_to_take_rt_mutex().
    
    Fixes: 3bfdc63936dd ("rtmutex: Use waiter::task instead of current in remove_waiter()")
    Reported-by: syzbot+78147abe6c524f183ee9@syzkaller.appspotmail.com
    Signed-off-by: Davidlohr Bueso <dave@stgolabs.net>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: stable@vger.kernel.org
    Closes: https://lore.kernel.org/all/69f114ac.050a0220.ac8b.0003.GAE@google.com/
    Link: https://patch.msgid.link/20260507112913.1019537-1-dave@stgolabs.net
    (cherry picked from commit 40a25d59e85b3c8709ac2424d44f65610467871e)
    
    Orabug: 39426001
    
    Conflicts:
    	 kernel/locking/rtmutex.c - Conflicts due to heavy code base
    differences, didn't try to pull in prerequisites, instead adapt the fix
    to older APIs instead.
    	kernel/locking/rtmutex_api.c - UEK6U3 doesn't have this file,
    take chane to rtmutex.c in the rt_mutex_start_proxy_lock() function.
    
    Signed-off-by: Harshit Mogalapalli <harshit.m.mogalapalli@oracle.com>
    Reviewed-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -1083,6 +1083,9 @@ static void remove_waiter(struct rt_mutex *lock,
 	struct task_struct *waiter_task = waiter->task;
 	struct rt_mutex *next_lock;
 
+	if (!waiter_task) /* never enqueued */
+		return;
+
 	raw_spin_lock(&waiter_task->pi_lock);
 	rt_mutex_dequeue(lock, waiter);
 	waiter_task->pi_blocked_on = NULL;
@@ -1693,7 +1696,7 @@ int rt_mutex_start_proxy_lock(struct rt_mutex *lock,
 		ret = 0;
 	}
 
-	if (unlikely(ret))
+	if (unlikely(ret < 0 ))
 		remove_waiter(lock, waiter);
 
 	raw_spin_unlock_irq(&lock->wait_lock);
